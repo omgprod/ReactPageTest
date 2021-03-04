3b49461008410a5a4b2ebbdd2f737f0afa60cc5a
BabaHagg <brian.haggey@epitech.eu>
2021-03-04 13:08:15 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Pages/Projects.js';
+import Articles from './Pages/Articles.js';
+import About from './Pages/About.js';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <div className="navigation">
+            <img src={logo} className="logo" alt="Logo Image" />
+            <div className="navigation-sub">
+
+              <Link to="/" className="item">Projects</Link>
+              <Link to="/articles" className="item">Articles</Link>
+              <Link to="/about" className="item">About</Link>
+
+            </div>
+          </div>
+          <div className="App-header">
+            <Route exact path="/" component={Projects} />
+            <Route path="/articles" component={Articles} />
+            <Route path="/about" component={About} />
+          </div>
+
+        </div>
+      </BrowserRouter>
   );
 }
 
