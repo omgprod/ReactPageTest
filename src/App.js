33c9ff2e8c2d7d94ca8d3fe4c46f60d70f110c01
BabaHagg <brian.haggey@epitech.eu>
2021-03-09 11:15:40 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -20,18 +20,16 @@ function App() {
             </div>
 
             <div className="navigation-sub">
-
               <Link to="/projects" className="item">Projects</Link>
               <Link to="/articles" className="item">Articles</Link>
               <Link to="/about" className="item">About</Link>
-
             </div>
           </div>
 
           <div className="App-header">
-            <Route exact path="/projects" component={Projects} />
-            <Route path="/articles" component={Articles} />
-            <Route path="/about" component={About} />
+            <Route exact path="/projects" component={ Projects } />
+            <Route path="/articles"       component={ Articles } />
+            <Route path="/about"          component={ About } />
           </div>
 
         </div>
